d0487ee700237854302ea667cd7883fc97e0726f
Hristo Jelev <jeleff@gmail.com>
2021-10-05 22:00:19 +0300

    wording

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 Bash script to create a wallpaper slideshow for GNOME
 
 I made this script to create a desktop wallpaper slideshow without Shotwell as this method was not working in Zorin OS.
+Calling gsettings directly from the command line will change your desktop wallpaper but when this command is called via a cronjob its not working properly.
+This script fixes this and can be called via a cronjob.
 
-To use the script update the DIR variable to point to your photos and then schedule it with a cron job
+To use the script update the DIR variable to point to your photos and then schedule it with a cron job.
 
 The cron job below will change your wallpaper every 10 minutes.
 
--- change_wallpaper.sh
+++ change_wallpaper.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
-# Get the Real Username
-RUID=$(who | awk 'FNR == 1 {print $1}')
-
-# Translate Real Username to Real User ID
-RUSER_UID=$(id -u ${RUID})
+# Bash script that changes your desktop wallpaper by picking a random one from the DIR folder
+# Schedule this script with a cronjob to get a wallpaper slideshow
 
 DIR="/home/masoko/Pictures/wallpapers/reddit"
+RUID=$(who | awk 'FNR == 1 {print $1}')
+RUSER_UID=$(id -u ${RUID})
 PIC=$(ls $DIR/* | shuf -n1)
-
-
-# Set gsettings for the Real User
 sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.gnome.desktop.background picture-uri "file://$PIC"
-
-# /usr/bin/gsettings set org.gnome.desktop.background picture-uri "file://$PIC"
 exit
