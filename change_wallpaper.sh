14119fc958c87aeb9ba386726cdbc20d85bb8908
Hristo Jelev <jeleff@gmail.com>
2022-01-01 22:47:48 +0200

    save PIC variable to a file

--- change_wallpaper.sh
+++ change_wallpaper.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Bash script that changes your desktop wallpaper by picking a random one from the DIR folder
 # Schedule this script with a cronjob to get a wallpaper slideshow
-
 DIR="/home/masoko/Pictures/wallpapers/reddit"
 RUID=$(who | awk 'FNR == 1 {print $1}')
 RUSER_UID=$(id -u ${RUID})
 PIC=$(ls $DIR/* | shuf -n1)
 sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.gnome.desktop.background picture-uri "file://$PIC"
+echo $PIC > /home/masoko/git/zorin-os-wallpaper-slideshow/change_wallpaper.log
 exit
