39ae1b62a36e5a5bb7a5cede09342304bd6630c6
hjelev <jeleff@gmail.com>
2022-05-05 00:24:32 +0300

    add support for ubuntu 22.04 dark theme

--- change_wallpaper.sh
+++ change_wallpaper.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 # Bash script that changes your desktop wallpaper by picking a random one from the DIR folder
 # Schedule this script with a cronjob to get a wallpaper slideshow
-DIR="/home/masoko/Pictures/wallpapers/reddit"
+DIR="/home/masoko/nas/wallpapers/reddit"
 RUID=$(who | awk 'FNR == 1 {print $1}')
 RUSER_UID=$(id -u ${RUID})
 PIC=$(ls $DIR/* | shuf -n1)
 sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.gnome.desktop.background picture-uri "file://$PIC"
+sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.gnome.desktop.background picture-uri-dark "file://$PIC"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo $PIC > $SCRIPT_DIR/change_wallpaper.log
 exit
